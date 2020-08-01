c9489ef078ee960296513b086e1fc12517066f0f
Raveen K Sharma <raveen08@gmail.com>
2020-08-01 20:00:23 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,4 +1,4 @@
-class Artist
+class Doctor
   attr_accessor :name
  
   @@all = []
@@ -18,7 +18,7 @@ class Artist
     
   def songs  
     Song.all.select do |song|
-      song.artist == self
+      song.doctor == self
     end
   end
   
